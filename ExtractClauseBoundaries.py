261ca5e41e891ea32f66fa05fcede19ecf9de982
hsimpson22 <hsimpson22@users.noreply.github.com>
2015-10-09 14:58:46 -0700

    Added code to make list of words extracted from Stanford parses equal to the GS_Score dataframe word column that it will be incorporated into

--- ExtractClauseBoundaries.py
+++ ExtractClauseBoundaries.py
@@ -55,21 +55,26 @@ tags = []
 ruleset=[]
 ClauseBoundary = False #below, this variable will be set to TRUE if the rule begins with 'S' (clause boundary), or FALSE the rule contains a terminal 
 for rule in rules:
-    print counter   
+    print counter 
+    tags.append(str(rule.lhs())) #store left-hand side syntactic category, this will create some duplicates in the tag set since sometimes it was already included in right hand side of previous rule, but we don't need to care about that
     ruleset.append(rule)
     rightside = rule.rhs() #
     if type(rightside[0])==str:
-        word = rightside[0] #store the word so it can be added to words output list
-        mini_cfg=CFG(Nonterminal("ROOT"), ruleset)
-        if mini_cfg.is_leftcorner(Nonterminal('S'), word):
-            ClauseBoundary = True
-        words.append((tags, word,ClauseBoundary))
-        tags = [] 
-        ruleset = []
-        ClauseBoundary = False   
+        if rightside[0] in ['.','?','\\/', '-LCB-','-RCB-','FOOTSTEPS']: #remove bad ones that I identified in previous run of this script
+          tags = [] 
+          ruleset = []
+          ClauseBoundary = False
+        else:
+            word = rightside[0] #store the word so it can be added to words output list
+            mini_cfg=CFG(Nonterminal("ROOT"), ruleset)
+            if mini_cfg.is_leftcorner(Nonterminal('S'), word):
+                ClauseBoundary = True
+            words.append((tags, word,ClauseBoundary))
+            tags = [] 
+            ruleset = []
+            ClauseBoundary = False   
     else: 
 # if the rule is not terminal (meaning rhs is another syntactic category tag like "NP")
-        tags.append(str(rule.lhs())) #store left-hand side syntactic category, this will create some duplicates in the tag set since sometimes it was already included in right hand side of previous rule, but we don't need to care about that
         tags.extend([str(r) for r in rightside])
     counter +=1
             
@@ -77,15 +82,158 @@ for rule in rules:
 #%%
         
 #==============================================================================
-# Identify "Words" that were split up by the parser (e.g. "they'll" and put them back together    
+# Identify Words that were split up by the parser: 
+        #contractions with apostrophes "they'll"
+        #modals "gonna" , "wanna" 
+   #put them back together
 #==============================================================================
-        
-mycfg.is_leftcorner(Nonterminal("S"), "I")
+position = []
+count = 0
+fixedcontractions = []
+contractions = []
+for w in words:  #so we are editing the list created in previous cell, that might be bad practice? but efficient bc it doesn't require creation of new list with almost all the same elements
+  if re.search(r'[^o]?\'[^c]', w[1]) or re.search(r'^na$', w[1]): #match the apostrophe in split contractions, the NOT 'o' and NOT 'c' are there to exclude 'o'clock'   
+    combinedtags = words[count-1][0]+w[0]
+    combinedwords = words[count-1][1]+w[1]
+    if words[count-1][2] or w[2]:
+      combinedclause = True #Set the Clause boundary status to "True" if either of the elements are True
+    else: 
+      combinedclause = False
+    newtuple = (combinedtags,combinedwords,combinedclause)
+    words[count-1]=newtuple # replace tuple at the previous position (first half of the contraction) with new joined elements, will remove the 2nd half of the contraction later
+    fixedcontractions.append(newtuple) #save the new tuples for review
+    contractions.append(w) #save list of contractions to use them in later removal step (matching the value in words)
+    position.append(count)
+  count+=1
+  
+#Editing the Words list to remove elements  
+#Remove Contractions
+wl = len(words)
+[words.remove(c) for c in contractions] # remove the second half of the contraction from the list 
+if wl-len(words)!=len(contractions): #check to make sure it removed correct amount
+    print ("Removal step removed %d, should have removed %d" % wl-len(words), len(contractions))
+
+#%% 
+#==============================================================================
+# Check for any additional not matching words
+#==============================================================================
+scores = pd.read_csv('./GS_Score_byWord_updated.csv', sep='\t') #The file containing the rest of the experiment data, we need to make sure that the list of words we've extracted from the syntactic parses matches the list of words here, so that we can match up their values for ClauseBoundary 
+bysubj = scores.groupby('Subj')
+subj0 = bysubj.get_group(0)
+scores_words = subj0['Word'].values 
+#len(scores_words) is smaller than len(words), so we still probably have some funky stuff going on (probably leftover punctuation that was treated as strings)
+#%%
+#Let's compare to see what remaining word strings are in words that aren't in scores_words
+scores_set = set(scores_words) 
+diffwords=[y for x,y,z in words if y not in scores_set];diffwords
+
+#['o',
+# 'threequarterinch',
+# 'threequarter',
+# 'thirtyone',
+# 'seventytwo',
+# 'twentyeight',
+# 'ninetyeights',
+# 'ninetysixes',
+# 'ninetynine']
+
+#==============================================================================
+# Fix the rest of not matching words:
+  #1. 'o' in stimulus 4 "fear is so crippling", in original text it was -o but clearly sounds like 'so' so I changed it in the scoring representation
+  
+  #2. multi-digit numbers that are treated as one word in parsed stimuli and multiple words in stimulus text (e.g. 'threequarterinch', 'seventytwo')
+    
+#==============================================================================
+#%%
+#First fix 'o' -> 'so' in Stim 4 
+[i for i,x in enumerate(words) if x[1] =='o'] # found 'o' is at position [341]
+words[341]=(words[341][0], 'so' , words[341][2]) #have to replace the entire tuple since we can't edit tuple elements
+
+#Update diffwords
+diffwords=[y for x,y,z in words if y not in scores_set]; diffwords
+# 'threequarterinch',
+# 'threequarter',
+# 'thirtyone',
+# 'seventytwo',
+# 'twentyeight',
+# 'ninetyeights',
+# 'ninetysixes',
+# 'ninetynine']
+
+#Fix number elements, need to split up into multiple entries
+
+#find values for the elements to fix
+fix_num_values = [x for x in words if x[1] in diffwords]
+
+#make sure the resulting list of positions has a one-to-one correspondence with diffwords
+if len(fix_num_values) != len(diffwords): 
+    print "Lengths of lists to fix do not match"  
+
+#If nothing, then we can move on to fixing these values
+#Note that we have to search for each position separately as we start changing values because we will be messing with the number of elements in the list. 
+
+#%%
+#Let's define a little function to split and insert new elements 
+def SplitValues(position, values, wordslist): #*value would be list of new values for 
+    newtuple =(wordslist[position][0], values[0], wordslist[position][2]) 
+    wordslist[position] = newtuple
+    if len(values)>1:
+        for i in range(1,len(values)):
+            nexttuple = (newtuple[0], values[i], newtuple[2])
+            wordslist.insert(position+i, nexttuple)    
+
+#%%
+
+#==============================================================================
+# Fix number values 
+
+#I'm stepping through each one by one, I didn't do this with a loop because I wanted to  be really careful and clear about the values that I'm changing instead of making it one big list of lists that would be hard to read
+#==============================================================================
+
+# 'threequarterinch'
+pos = [i for i, x in enumerate(words) if x[1] =='threequarterinch'][0] 
+SplitValues(pos, ['three', 'quarter', 'inch'], words)
+
+# 'threequarter',
+pos = [i for i, x in enumerate(words) if x[1] =='threequarter'][0] 
+SplitValues(pos, ['three', 'quarter'], words)
+# 'thirtyone',
+pos = [i for i, x in enumerate(words) if x[1] =='thirtyone'][0] 
+SplitValues(pos, ['thirty', 'one'], words)
+
+# 'seventytwo',
+pos = [i for i, x in enumerate(words) if x[1] =='seventytwo'][0] 
+SplitValues(pos, ['seventy', 'two'], words)
+
+# 'twentyeight',
+pos = [i for i, x in enumerate(words) if x[1] =='twentyeight'][0] 
+SplitValues(pos, ['twenty', 'eight'], words)
+
+# 'ninetyeights',
+pos = [i for i, x in enumerate(words) if x[1] =='ninetyeights'][0] 
+SplitValues(pos, ['ninety', 'eights'], words)
+
+# 'ninetysixes',
+pos = [i for i, x in enumerate(words) if x[1] =='ninetysixes'][0] 
+SplitValues(pos, ['ninety', 'sixes'], words)
+
+# 'ninetynine']
+pos = [i for i, x in enumerate(words) if x[1] =='ninetynine'][0] 
+SplitValues(pos, ['ninety', 'nine'], words)
+
+#check on word diffs again
+diffwords=[y for x,y,z in words if y not in scores_set]
+if bool(diffwords) == False: 
+    print "No diffs between words and set(GS_scores_words)"
+#There are no diffs!
+
+#Now check for differences the other way, see if there are any words in GSscores that aren't in words set
+s2 = set([y for x,y,z in words])
+diffwords2 = [y for w in scores_words if w not in s2]
+if bool(diffwords2)==False:
+    print "No diffs between Clause boundary word set and GS Scores word set! Proceed to next step!"
+#also empty!
+
+
 #Output Clause begin status for each word (YES if it's the first word after the S tag) 
 #I will do one with SBAR and one with S.. SBAR indicates a complementizer or adverbial that introduces a subordinate clause, the SBAR node may be empty
-
-
-
-#Beginning of Stimulus marked as so: 
-
-#have to join any separated contractions, e.g. "That   's"
